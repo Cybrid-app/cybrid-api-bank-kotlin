4e83c4629b0209793f423af27c7f9ddaf09c5af5
brent-cybrid <brent@cybrid.app>
2022-06-15 18:18:58 +0000

    v0.27.0

--- src/main/kotlin/app/cybrid/cybrid_api_bank/client/apis/SymbolsApi.kt
+++ src/main/kotlin/app/cybrid/cybrid_api_bank/client/apis/SymbolsApi.kt
@@ -5,6 +5,7 @@ import retrofit2.http.*
 import retrofit2.Response
 import okhttp3.RequestBody
 
+import app.cybrid.cybrid_api_bank.client.models.ErrorResponseBankModel
 
 interface SymbolsApi {
     /**
@@ -12,6 +13,9 @@ interface SymbolsApi {
      * Retrieves a listing of symbols.  Required scope: **prices:read**
      * Responses:
      *  - 200: get list of symbols
+     *  - 400: Invalid requests - Malformed Authentication Header
+     *  - 401: Unauthorized - Authentication failed, 
+     *  - 403: Invalid scope
      *
      * @return [kotlin.collections.List<kotlin.String>]
      */
